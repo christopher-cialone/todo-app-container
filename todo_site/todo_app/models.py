0704cf5589fbfcfc2efdf44a00a381c248e6c185
Christopher Cialone <ccialone0929@gmail.com>
2022-12-30 14:49:19 -0500

    added forms

--- todo_site/todo_app/models.py
+++ todo_site/todo_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
+# This is what creates the structure of our DB
 class Task(models.Model):
     description = models.CharField(max_length=255)
